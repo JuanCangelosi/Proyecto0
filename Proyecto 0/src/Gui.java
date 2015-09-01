54a9c97bcf13a0227d9237430dd1f891a1b77d96
Leandro <Leandro@172.16.0.12>
2015-08-31 23:15:48 -0300

    Arreglo del gif pero la dimension de la image es pequeña, re organiacion de la GUI

--- Proyecto 0/src/Gui.java
+++ Proyecto 0/src/Gui.java
@@ -7,44 +7,53 @@ import java.awt.event.ActionListener;
 
 public class Gui extends JPanel {
 	private JPanel panelImagen;
-	private JPanel panelTexto;
+	private JPanel panelTexto, panelSur;
 	private JButton boton;
 	private JFrame frame;
 	private JLabel imagen;
 	private JLabel texto;
+	private JTextPane texto1;
 	public static void main(String[] args) {
 		Gui window = new Gui();
 		window.frame.setVisible(true);
 	}
 	public Gui(){
 		frame = new JFrame("The simpsons");
-		frame.setSize(new Dimension(500,400));
+		frame.setSize(new Dimension(500,385));
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout());
 		
 		panelTexto=new JPanel();
-		texto=new JLabel();
 		panelImagen=new JPanel();
-		Image img = new ImageIcon(this.getClass().getResource(
-				"/Imagenes/Ralph.jpg")).getImage();
-		imagen=new JLabel(new ImageIcon(img));
+		ImageIcon img = new ImageIcon(this.getClass().getResource(
+				"/Imagenes/Ralph.jpg"));
+		imagen=new JLabel(img);
 		panelImagen.add(imagen);
-		panelTexto.add(texto);
 		
-		boton=new JButton("Presione Aqui");
+		//Panel Sur contiene cuadro de texto y botonera
+		panelSur= new JPanel();
+		panelSur.setLayout(new GridLayout(1,2));
+		texto1= new JTextPane();
+		texto1.setEditable(false);
+		panelSur.add(texto1);
+		boton=new JButton("PRESIONAME");
+		boton.setBackground(Color.WHITE);
 		boton.addActionListener(new OyenteBoton());
+		panelSur.add(boton);
+		
 		frame.add(panelTexto);
 		frame.add(panelImagen, BorderLayout.CENTER);
-		frame.add(boton, BorderLayout.SOUTH);
+		frame.add(panelSur, BorderLayout.SOUTH);
 		
 		}
 	private class OyenteBoton implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			Image img1 = new ImageIcon(this.getClass().getResource(
-					"/Imagenes/Ralph Gif.gif")).getImage();
-			imagen.setIcon(new ImageIcon(img1));
-			texto.setText("Hola super nintendo Chalmers");
-			panelTexto.add(texto);
+			ImageIcon img1 = new ImageIcon(this.getClass().getResource(
+					"/Imagenes/Ralph2.gif"));
+			texto1.setText("Hi, Super Nintendo Chalmers.");
+			imagen.setIcon(img1);
 		}
 	}
 }
