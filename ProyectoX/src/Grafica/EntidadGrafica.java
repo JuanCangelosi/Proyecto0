2b1e745469cca0bbf564cfbe806f37d35acc2f50
luchofuentes <lucho.fuentez@gmail.com>
2015-10-21 19:59:06 -0300

    Subo EntidadGrafica

---
+++ ProyectoX/src/Grafica/EntidadGrafica.java
@@ -0,0 +1,20 @@
+package Grafica;
+
+import javax.swing.JComponent;
+
+public abstract class EntidadGrafica extends JComponent{
+	
+	private static final long serialVersionUID = 1L;
+	protected final int width = 32;
+	protected final int height = 32;
+	protected int velocidad;
+	protected int x,y;
+	
+	protected EntidadGrafica(int x, int y) {
+		this.x = x;
+		this.y = y;
+		setBounds(x,y,width,height);
+	}
+	
+
+}
