b61808b9b9b16143a63622f608fea948169fc123
Leandro <Leandro@Lea>
2015-11-24 12:59:00 -0300

    nada

--- ProyectoX/src/Threads/ThreadMusica.java
+++ ProyectoX/src/Threads/ThreadMusica.java
@@ -1,28 +1,38 @@
 package Threads;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
 import Sonidos.*;
+
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
+import org.omg.CORBA.portable.InputStream;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 public class ThreadMusica extends Thread {
 //Atributos de instancia
 	private Clip sonido;
 	public ThreadMusica(){
 		try {
-			sonido = AudioSystem.getClip();
-			File a = new File("/Sonidos/Electrodynamix.wav");
-			sonido.open(AudioSystem.getAudioInputStream(a));
-			}
+			/*sonido = AudioSystem.getClip();
+			BufferedInputStream bis= new BufferedInputStream(getClass().getResourceAsStream("/Sonidos/Electrodynamix.wav"));
+			AudioInputStream ais= AudioSystem.getAudioInputStream(bis);
+			sonido.open(ais);*/
+		}
 			catch (Exception tipoerror) {
 			System.out.println("" + tipoerror);
 			}
 	}
 	
 	public void run(){
-		while(true){
+		while(true){ 
 			//try{
-			sonido.start();
+			//sonido.start();
 			/*sleep(205000);
 			sonido.close();
 			}catch(Exception e){}*/
